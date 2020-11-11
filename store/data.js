835f7bb5fed89621c246ea7ab04d98870ed28eb0
Malek Smida <msmida96@gmail.com>
2020-11-11 21:45:48 +0100

    #Feat: store initial data

---
+++ store/data.js
@@ -0,0 +1,58 @@
+// local data to start with, helps understand more application features
+const todoList = [
+  {
+    id: 0,
+    name: "Workout",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Pull ups",
+        completed: false,
+      },
+      {
+        title: "Push ups",
+        completed: true,
+      },
+      {
+        title: "Running",
+        completed: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Reading",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "The war of art",
+        completed: true,
+      },
+      {
+        title: "How to win friends and influence people",
+        completed: true,
+      },
+      {
+        title: "Think and grow rich",
+        completed: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Food",
+    color: "#5958D9",
+    todos: [
+      {
+        title: "Chicken",
+        completed: false,
+      },
+      {
+        title: "Fish",
+        completed: false,
+      },
+    ],
+  },
+];
+
+export default todoList;
